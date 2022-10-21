b1fbbfca6822340fd4de924c1d5387177ff66e53
Mingle <ggydm@vip.qq.com>
2022-10-21 10:04:53 +0800

    update

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+find ../ -name ".DS_Store" -depth -exec rm {} \;
+
+# for file in `ls ../projects`
+#     do
+#       if [[ -d "../projects/$file" ]]; then
+#         mkdir -p "./debs/$file"
+#         dpkg-deb -bZgzip "../projects/$file" "./debs/$file"
+# 			fi
+#     done
+
+dpkg-scanpackages -m ./debs > ./Packages
+bzip2 -fks ./Packages
+
+git add .
+git commit -m "update cydia repo"
+git push
